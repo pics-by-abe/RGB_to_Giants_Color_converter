c9cd4d16a416f9a8778498ea16de39c48b900153
unknown <abe@bayern-agrar.de>
2021-05-29 16:05:16 +0200

    fixed calculation

--- RGB_to_Giants_color_converter/Form1.cs
+++ RGB_to_Giants_color_converter/Form1.cs
@@ -17,31 +17,68 @@ namespace RGB_to_Giants_Color_Converter
             InitializeComponent();
         }
 
+        string RintFinal;
+
+        string GintFinal;
+
+        string BintFinal;
+
         private void btnConvert_Click(object sender, EventArgs e)
         {
             string Rtxt = txtColorR.Text;
             int Rint = int.Parse(Rtxt);
-            double Rint1 = (double)(Rint) / (double)(255);
-            double Rfinal = Math.Round(Math.Pow(Rint1, (1 / 2.2)), 3);
-            string Rfinalstr = Rfinal.ToString().Replace(",", ".");
+
+            if (((double)Rint / (double)255) < 0.04045)
+            {
+                double Rint1 = Rint / 255;
+                RintFinal = (Rint1 * 12.92).ToString().Replace(",", ".");
+            }
+            else
+            {
+                double Rint1 = (double)Rint / (double)255;
+                double Rint2 = Rint1 + 0.055;
+                double Rint3 = (double)Rint2 / (double)1.055;
+                double Rint4 = Math.Round(Math.Pow(Rint3, 2.4), 3);
+                RintFinal = Rint4.ToString().Replace(",", ".");
+            }
 
 
             string Gtxt = txtColorG.Text;
             int Gint = int.Parse(Gtxt);
-            double Gint1 = (double)(Gint) / (double)(255);
-            double Gfinal = Math.Round(Math.Pow(Gint1, (1 / 2.2)), 3);
-            string Gfinalstr = Gfinal.ToString().Replace(",", ".");
 
+            if (((double)Gint / (double)255) < 0.04045)
+            {
+                double Gint1 = Gint / 255;
+                GintFinal = (Gint1 * 12.92).ToString().Replace(",", ".");
+            }
+            else
+            {
+                double Gint1 = (double)Gint / (double)255;
+                double Gint2 = Gint1 + 0.055;
+                double Gint3 = (double)Gint2 / (double)1.055;
+                double Gint4 = Math.Round(Math.Pow(Gint3, 2.4), 3);
+                GintFinal = Gint4.ToString().Replace(",", ".");
+            }
+            
 
 
             string Btxt = txtColorB.Text;
             int Bint = int.Parse(Btxt);
-            double Bint1 = (double)(Bint) / (double)(255);
-            double Bfinal = Math.Round(Math.Pow(Bint1, (1 / 2.2)), 3);
-            string Bfinalstr = Bfinal.ToString().Replace(",", ".");
 
-
-            txtOutPut.Text = Rfinalstr + " " + Gfinalstr + " " + Bfinalstr;
+            if (((double)Bint / (double)255) < 0.04045)
+            {
+                double Bint1 = Bint / 255;
+                BintFinal = (Bint1 * 12.92).ToString().Replace(",", ".");
+            }
+            else
+            {
+                double Bint1 = (double)Bint / (double)255;
+                double Bint2 = Bint1 + 0.055;
+                double Bint3 = (double)Bint2 / (double)1.055;
+                double Bint4 = Math.Round(Math.Pow(Bint3, 2.4), 3);
+                BintFinal = Bint4.ToString().Replace(",", ".");
+            }
+            txtOutPut.Text = $"{RintFinal} {GintFinal} {BintFinal}";
 
         }
 
